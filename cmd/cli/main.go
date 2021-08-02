0b9643417613c9e49352a5beb917d79219b5829a
charlie <marmot00@gmail.com>
2021-08-03 00:19:35 +0800

    trace redirect requests

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,16 +8,15 @@ import (
 
 func main() {
 	//default
-	//knocker https://daniu.cool@45.60.64.140
-	//knocker https://daniu.cool@8.212.8.138:7788
 
-	rawURL := "http://daniu.cool/admin/login/index.html"
+	//rawURL := "http://daniu.cool/admin/login/index.html"
+	rawURL := "http://daniu.cool"
+	//rawURL := "https://image06.fenhao24.com:443/"
 	//modifiedIP := "45.60.64.140"
-	//modifiedIP := "52.229.224.88"
+	modifiedIP := "52.229.224.88"
 	//port := 443
 
-	//statusCode, err := knocker.Knock2(rawURL, modifiedIP, 0, true)
-	statusCode, err := knocker.Knock2(rawURL, "", 0, true)
+	statusCode, err := knocker.Knock2(rawURL, modifiedIP, 0, true)
 	failOnError(err, "failed to knock")
 
 	log.Printf("got status %d\n", statusCode)
--- knocker.go
+++ knocker.go
@@ -17,13 +17,13 @@ func Knock2(site, ip string, port int, withTrace bool) (statusCode int, err erro
 		return 0, fmt.Errorf("failed to Parse URL: %v", err)
 	}
 
-	dialer := &net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-	}
-
 	// replace ip only if specified
 	if ip != "" {
+		dialer := &net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}
+
 		http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
 			if addr == url.Host+":80" {
 				addr = ip + ":80"
@@ -31,6 +31,7 @@ func Knock2(site, ip string, port int, withTrace bool) (statusCode int, err erro
 			if addr == url.Host+":443" {
 				addr = ip + ":443"
 			}
+
 			return dialer.DialContext(ctx, network, addr)
 		}
 	}
@@ -40,36 +41,56 @@ func Knock2(site, ip string, port int, withTrace bool) (statusCode int, err erro
 		return 0, fmt.Errorf("failed to http.NewRequest: %v", err)
 	}
 
+	t := &transport{}
 	if withTrace {
 		trace := &httptrace.ClientTrace{
 			DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
-				fmt.Printf("DNS Info: %+v\n", dnsInfo)
+				printDebugHeader("DNS resoling...")
+				fmt.Printf("DNS resolve result: %+v\n", dnsInfo.Addrs)
 			},
-			GotConn: func(connInfo httptrace.GotConnInfo) {
-				fmt.Printf("Got Conn: %+v\n", connInfo)
-			},
+			GotConn: t.printConnInfo,
 		}
 		request = request.WithContext(httptrace.WithClientTrace(request.Context(), trace))
 	}
 
-	response, err := http.DefaultClient.Do(request)
+	client := &http.Client{Transport: t}
+
+	response, err := client.Do(request)
 	if err != nil {
 		return 0, fmt.Errorf("failed do a request: %v", err)
 	}
 
-	//printStatusCode(response.StatusCode)
-	//printSeparateLine()
-	//printHeader(response.Header)
+	printDebugHeader("request completed")
 
 	return response.StatusCode, nil
 }
 
+type transport struct {
+	current *http.Request
+}
+
+func (t *transport) printConnInfo(info httptrace.GotConnInfo) {
+	// we can use request info here
+	printDebugHeader("GotConn")
+	fmt.Printf("request %s @ %s...\n", t.current.URL, info.Conn.RemoteAddr())
+}
+
+func (t *transport) RoundTrip(request *http.Request) (*http.Response, error) {
+	t.current = request
+	resp, err := http.DefaultTransport.RoundTrip(request)
+
+	printDebugHeader("RoundTrip")
+	printStatusCode(resp.StatusCode)
+	printHeader(resp.Header)
+
+	return resp, err
+}
 func printStatusCode(code int) {
 	fmt.Printf("Status Code: %d\n", code)
 }
 
-func printSeparateLine() {
-	fmt.Printf("---\n")
+func printDebugHeader(msg string) {
+	fmt.Printf("\n### %s ###\n", msg)
 }
 
 func printHeader(header http.Header) {
